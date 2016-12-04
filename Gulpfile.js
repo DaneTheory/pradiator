fb1e6e0ac433defb03fe691004c22b5c661b3e98
DaneTheory <branden.dane@reaktor.com>
2016-12-04 03:09:13 -0500

    Provides working default Gulp task example

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,4 +4,5 @@ var gulp = require('gulp');
             Default Gulp Task
 =======================================*/
 gulp.task('default', function () {
+    console.log('Gulp Default Task Is Running!');
 });
