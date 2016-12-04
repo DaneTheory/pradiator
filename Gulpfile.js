db843846cc65b265eea872f17fd7ad0b7c7452f8
DaneTheory <branden.dane@reaktor.com>
2016-12-04 06:19:37 -0500

    Fixes gulp scripts task support
    The `scripts` gulp task now provides proper linting and uglification
    which compiles down to a single file for use in index.html. Alongside
    script optimization tasks, gulp `scripts` now supports ES6 uglification
    by means of the newly required module, gulp-babel and it’s dependency
    babel-preset-es2015. Both can be found within the package.json.
    
    The .jshintrc file has been modified to support vendor scripts
    referenced within index.html via CDN’s. These custom vendor Classes can
    be edited in .jshintrc via “globals”. Also, the ECMAScript version has
    been updated from 5 to 6 now that gulp-babel is handling script
    transpile.
    
    A minor but import tweak was made to the index.html file. In line with
    best practices for optimal front-end performance, all scripts (save for
    modernizer) are now called before the closing of the body tag.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
     uglify = require('gulp-uglify'),
@@ -25,13 +26,16 @@ gulp.task('styles', function () {
             Scripts Task
 =======================================*/
 gulp.task('scripts',function(){
-  gulp.src('')
+  gulp.src('src/scripts/scripts.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(babel({
+      presets: ["es2015"]
+    }))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest('app/assets/js'));
 });
 
 
@@ -40,4 +44,5 @@ gulp.task('scripts',function(){
 =======================================*/
 gulp.task('default', ['styles', 'scripts'], function () {
     gulp.watch("src/scss/*/*.scss", ['styles']);
+    gulp.watch("src/js/*.js", ['scripts']);
 });
