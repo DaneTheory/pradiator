776e2490f78a95ce2e4a131861e058e3567ec698
DaneTheory <branden.dane@reaktor.com>
2016-12-04 03:29:21 -0500

    Adds Gulp task for handling styles
    The `styles` gulp task provides an alternative to using pure CSS for
    styling by introducing SASS to the project. All SASS files will compile
    down to a single CSS file any time the `default` gulp task is run.
    
    Included alongside SASS during compilation are a few gulp modules that
    take care of basic front-end optimization techniques. These are
    gulp-autoprefixer and gulp-cssnano. To better serve a stronger
    development work flow, all compiled styles are renamed and added to a
    separate folder location. This is a typical automated task technique
    for working with SASS and it’s optimization counterparts. To aid this
    process, gulp-rename was included as well.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano');
+
+
+/*======================================
+            Styles Task
+=======================================*/
+gulp.task('styles', function () {
+    return gulp.src('')
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(gulp.dest(''))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(''));
+});
 
 /*======================================
             Default Gulp Task
 =======================================*/
-gulp.task('default', function () {
+gulp.task('default', ['styles'], function () {
     console.log('Gulp Default Task Is Running!');
 });
